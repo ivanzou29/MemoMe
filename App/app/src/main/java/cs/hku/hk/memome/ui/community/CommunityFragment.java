95bf4346c1c439b1181f19cbdcadcdad9eb1c641
YiyanChen <yiyan17@hku.hk>
2019-11-03 16:54:33 +0800

    some changes to the layouts

--- App/app/src/main/java/cs/hku/hk/memome/ui/community/CommunityFragment.java
+++ App/app/src/main/java/cs/hku/hk/memome/ui/community/CommunityFragment.java
@@ -27,7 +27,7 @@ public class CommunityFragment extends Fragment {
         communityViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
