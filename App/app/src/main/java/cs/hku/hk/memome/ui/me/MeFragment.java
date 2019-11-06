e32a9589f4c2865233b789984894c73816bc3d82
YiyanChen <yiyan17@hku.hk>
2019-11-06 21:18:48 +0800

    add click listener

--- App/app/src/main/java/cs/hku/hk/memome/ui/me/MeFragment.java
+++ App/app/src/main/java/cs/hku/hk/memome/ui/me/MeFragment.java
@@ -1,9 +1,14 @@
 package cs.hku.hk.memome.ui.me;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +17,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import cs.hku.hk.memome.GiftsActivity;
+import cs.hku.hk.memome.ProfileActivity;
 import cs.hku.hk.memome.R;
 
 public class MeFragment extends Fragment {
@@ -28,6 +35,20 @@ public class MeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s);
+            }
+        });
+        Button gift = root.findViewById(R.id.gifts_button_in_me);
+        ImageButton me = root.findViewById(R.id.profile_icon_button);
+        gift.setOnClickListener(new OnClickListener(){
+            public void onClick(View view){
+                Intent myIntent = new Intent(view.getContext(), GiftsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        me.setOnClickListener(new OnClickListener(){
+            public void onClick(View view){
+                Intent myIntent = new Intent(view.getContext(), ProfileActivity.class);
+                startActivity(myIntent);
             }
         });
         return root;
