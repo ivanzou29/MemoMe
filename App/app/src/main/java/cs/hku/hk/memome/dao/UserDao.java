f90a281ccbfe6269008699c34e4bdd7a6fd929b7
ivanrz29 <ivanzou29@gmail.com>
2019-11-09 18:29:05 +0800

    SQL first trail

--- App/app/src/main/java/cs/hku/hk/memome/dao/UserDao.java
+++ App/app/src/main/java/cs/hku/hk/memome/dao/UserDao.java
@@ -1,4 +1,7 @@
 package cs.hku.hk.memome.dao;
 
+import cs.hku.hk.memome.model.User;
+
 public interface UserDao {
+    User getUserFromEmail(String email);
 }
--- App/app/src/main/java/cs/hku/hk/memome/jdbc/UserJdbcDao.java
+++ App/app/src/main/java/cs/hku/hk/memome/jdbc/UserJdbcDao.java
@@ -1,4 +1,39 @@
 package cs.hku.hk.memome.jdbc;
 
-public class UserJdbcDao {
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import cs.hku.hk.memome.dao.UserDao;
+import cs.hku.hk.memome.database.DatabaseUtilities;
+import cs.hku.hk.memome.model.User;
+
+public class UserJdbcDao implements UserDao {
+
+
+    @Override
+    public User getUserFromEmail(String email) {
+
+        Connection conn = DatabaseUtilities.openConnection();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Users WHERE email = \"" + email + "\"");
+            rs.next();
+            User user = new User();
+            user.setEmail(email);
+            user.setCoin(rs.getInt("coin"));
+            user.setPasscode(rs.getString("passcode"));
+            user.setProfilePhoto(rs.getBytes("profile_photo"));
+            user.setUsername(rs.getString("Username"));
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
 }
